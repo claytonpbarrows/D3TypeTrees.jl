1c66d4fc9c4e4c46393725344dd69d9d6d901e71
Barrows <clayton.barrows@nrel.gov>
2019-02-07 14:59:20 -0700

    adding code

---
+++ src/D3TypeTree.jl
@@ -0,0 +1,68 @@
+
+isdefined(Base, :__precompile__) && __precompile__()
+
+"""
+Module for constructing self-contained power system objects.
+"""
+module D3TypeTree
+
+#################################################################################
+# Exports
+
+export TypeTree
+
+#################################################################################
+# Imports
+
+using JSON
+using D3Trees
+
+
+#################################################################################
+# Source
+
+function wrap(t::Type)
+	s = subtypes(t)
+	if isempty(s)
+		d = [string(fn) for fn in fieldnames(t)]
+	else
+		d = Dict()
+		for t_inner in s
+			if t_inner != t
+				d[string(t_inner)] =  wrap(t_inner)
+			end
+		end
+	end
+	return d
+end
+
+
+function get_children(d::Dict, c = Array{Array{Any,1},1}(),n = Array{String,1}(),f = Array{String,1}())
+	cc = Array{Any,1}()
+	for (k,v) in d
+		if isa(v,Dict) 
+			get_children(v,c,n,f)
+			push!(cc,String(k))
+		elseif isa(v,Array)
+			push!(c,[])
+			push!(cc,String(k))
+		end
+		if !(String(k) in n)
+			push!(n,String(k))
+			push!(f,isa(v,Array) ? join(v,"\n") : "Abstract")
+		end
+	end
+	push!(c,reverse(cc))
+	c = [[findall(x->x==i, reverse(n))[1] for i in j] for j in reverse(c)]
+	#c = reverse(c)
+	return c[2:end], reverse(n), reverse(f)
+end
+
+function TypeTree(t::Type;browser="google chrome",init_expand=10)
+	t = Dict(string(t)=>wrap(t))
+	children, names, fields = get_children(t)
+    tree = D3Tree(children,text = names,tooltip = fields,init_expand=10)
+    inbrowser(tree,browser)
+end
+
+end #module
